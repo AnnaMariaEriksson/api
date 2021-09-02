d4de857001dd8f51ce7f32308e6933e1b0e338fc
AnnaMaria Eriksson <ameriksson.se@icloud.com>
2021-09-02 14:38:08 +0200

    Changed something

--- app.js
+++ app.js
@@ -4,8 +4,6 @@ const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
-app.get('/dogs', (req, res) =>
-    res.send("Ludde, Lufsen, Molle, Micke")
-)
+app.get('/dogs', (req, res) => res.send("Ludde, Lufsen, Molle, Micke"))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
